1a029e070b053bba034bc0f67b3cdff40fc287ea
vietanh112 <vietanh.personal@gmail.com>
2023-04-09 23:28:52 +0700

    install package

--- install/setup_inv.sh
+++ install/setup_inv.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 
-sudo apt install texlive-science texlive-latex-extra texlive-extra-utils latexmk texlive-publishers texlive-science
+sudo apt install texlive-base texlive-science texlive-latex-extra texlive-extra-utils latexmk texlive-publishers
+sudo apt-get install texlive-lang-other -y && sudo texhash
+
+#https://ctan.org/pkg/biblatex
+sudo add-apt-repository universe && sudo apt-get update
+sudo apt-get install biblatex
+sudo texhash
